51c872fdf84c306bd6c40de628b26511ecf882b6
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2022-09-19 09:10:45 +0200

    oof

---
+++ 1909/1909/CreditAcc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1909
+{
+    internal class CreditAcc
+    {
+        private bool card;
+        private int limit;
+        private int id;
+        private int balance;
+
+        public CreditAcc(bool card, int limit, int id, int balance)
+        {
+            this.card = card;
+            this.limit = limit;
+            this.id = id;
+            this.balance = balance;
+        }
+
+        public bool payCard(int value)
+        {
+            if (!card) throw new ArgumentException("User doenst have card"); 
+            if (value<= limit )
+            {
+                balance = balance - value;
+            } 
+        }
+    }
+}
